de7015e530525ef882af0d91317651f7216feae3
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-07-24 14:04:59 +0200

    Update install_dependencies.sh
    version  to version 2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+
+set -e
+
+echo "Installing dependencies..."
+
 sudo yum install tomcat -y
+if [ $? -ne 0 ]; then
+  echo "Error installing Tomcat"
+  exit 1
+fi
+
 sudo yum -y install httpd
+if [ $? -ne 0 ]; then
+  echo "Error installing HTTPD"
+  exit 1
+fi
+
 sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
   ServerAdmin root@localhost
@@ -13,3 +28,9 @@ sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 </VirtualHost>
 EOF
 
+if [ $? -ne 0 ]; then
+  echo "Error creating tomcat_manager.conf file"
+  exit 1
+fi
+
+echo "Dependencies installed successfully"
